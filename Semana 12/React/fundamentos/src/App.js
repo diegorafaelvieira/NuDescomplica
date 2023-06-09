d3339d12357fe19712ea0e2aa2c26f2106ff2ec1
Diego Rafael Vieira <diegorafaelvieira@bol.com.br>
2023-06-09 00:02:15 -0300

    Add React Fundamentos 1

---
+++ Semana 12/React/fundamentos/src/App.js
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <div className="App">
+      <h1>
+        <span>Olá </span>
+        <strong>mundo</strong>
+        !!!
+      </h1>
+      <article>
+        <h2>Subtítulo</h2>
+        <p>Parágrafo</p>
+      </article>
+    </div>
+  );
+}
+
+export default App;
