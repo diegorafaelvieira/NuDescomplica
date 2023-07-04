e00824f67d4ce864cb3eec34eae3af9b919cbefd
Diego Rafael Vieira <diegorafaelvieira@bol.com.br>
2023-07-03 23:14:56 -0300

    Carregando dados dinâmicos na página /minhas-pastas

--- Semana 13/React/projeto/src/pages/MinhasPastas/MinhasPastasPage.js
+++ Semana 13/React/projeto/src/pages/MinhasPastas/MinhasPastasPage.js
@@ -1,17 +1,19 @@
 import Container from "react-bootstrap/Container";
 import { ListGroup } from "../../components/ListGroup/ListGroup";
+import { useAppContext } from "../../store/AppContext";
 
-export const MinhasPastas = () => (
+const adapterItems = (items) => {
+  return items.map(item => ({
+    title: item.name,
+    total: item.pins.length
+  }));
+};
+
+export const MinhasPastas = () => {
+  const { state } = useAppContext();
+  return (
     <Container>
-      <ListGroup items={[
-        {
-          title: 'Matemática',
-          total: 3
-        },
-        {
-          title: 'JavaScript',
-          total: 10
-        }
-      ]} />
+      <ListGroup items={adapterItems(state.folders)} />
     </Container>
-)
+  );
+};
